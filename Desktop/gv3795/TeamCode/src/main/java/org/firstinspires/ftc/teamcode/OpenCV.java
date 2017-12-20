52c505a099f39e0356f5735ca644be4074731e4e
Ben Loan <ben6501@gmail.com>
2017-12-20 14:01:27 -0500

    Added turn functions into robot that take the degrees as paramaters

---
+++ Desktop/gv3795/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by BenLoan on 10/3/17.
+ */
+
+public class OpenCV {
+}
---
+++ Desktop/gv3795/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by BenLoan on 10/3/17.
+ */
+
+public class Vuforia extends LinearOpMode {
+
+    
+}
